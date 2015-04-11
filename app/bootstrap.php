83c7962733fcc24aabc9f05dbe423106bab49dec
Tomáš Markacz <tomas.markacz@gmail.com>
2015-04-11 09:52:42 +0200

    refactor: seperate cofig files, shorter bootstrapping

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,21 +1,20 @@
 <?php
 
+use Nette\Application\Application;
+use Nette\Configurator;
+
 require __DIR__ . '/../vendor/autoload.php';
 
-$configurator = new Nette\Configurator;
+$configurator = new Configurator;
 
-//$configurator->setDebugMode('23.75.345.200'); // enable for your remote IP
 $configurator->enableDebugger(__DIR__ . '/../log');
-
 $configurator->setTempDirectory(__DIR__ . '/../temp');
+$configurator->createRobotLoader()->addDirectory(__DIR__)->register();
+$configurator->addConfig(__DIR__ . '/config/app.neon');
+$configurator->addConfig(__DIR__ . '/config/console.neon');
+$configurator->addConfig(__DIR__ . '/config/model.neon');
+$configurator->addConfig(__DIR__ . '/config/local.neon');
 
-$configurator->createRobotLoader()
-	->addDirectory(__DIR__)
-	->register();
+return $configurator->createContainer();
 
-$configurator->addConfig(__DIR__ . '/config/config.neon');
-$configurator->addConfig(__DIR__ . '/config/config.local.neon');
 
-$container = $configurator->createContainer();
-
-return $container;
