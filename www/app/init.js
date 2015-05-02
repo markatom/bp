c29fe93cfbb1dc681aef067c7f1c5491228b08fc
Tomáš Markacz <tomas.markacz@gmail.com>
2015-05-03 00:01:55 +0200

    refactoring: move aditional method of Date into main.js

--- www/app/init.js
+++ www/app/init.js
@@ -2,16 +2,6 @@
 
 define(['app/sections', 'app/welcome', 'app/order', 'app/user', 'app/client', 'app/order'], function () {
 
-    Date.prototype.toString = function () {
-        function pad(number) {
-            return number < 10 ? '0' + number : number;
-        }
-
-        return this.getFullYear()
-            + '-' + pad(this.getMonth() + 1)
-            + '-' + pad(this.getDate());
-    };
-
     angular.bootstrap(document.documentElement, ['app.welcome', 'app.sections', 'app.order', 'app.user', 'app.client', 'app.order']);
 
 });
--- www/js/main.js
+++ www/js/main.js
@@ -1,3 +1,13 @@
+Date.prototype.toString = function () {
+    function pad(number) {
+        return number < 10 ? '0' + number : number;
+    }
+
+    return this.getFullYear()
+        + '-' + pad(this.getMonth() + 1)
+        + '-' + pad(this.getDate());
+};
+
 requirejs.config({
     baseUrl: 'app',
     paths: {
