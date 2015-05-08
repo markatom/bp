37fa26c049a98fd4f0a55e193f2c741de8bde961
Tomáš Markacz <tomas.markacz@gmail.com>
2015-05-08 23:00:45 +0200

    remove unused templates and states

--- www/app/sections.js
+++ www/app/sections.js
@@ -31,14 +31,6 @@ define(['app/welcome', 'app/gui'], function () {
                 })
                 .state('loading', {
                     templateUrl: 'app/sections/loading.html'
-                })
-                .state('downloading', {
-                    url: '/downloading',
-                    templateUrl: 'app/sections/downloading.html'
-                })
-                .state('downloaded', {
-                    url: '/downloaded',
-                    templateUrl: 'app/sections/downloaded.html'
                 });
 
             $urlRouterProvider.otherwise('/');
